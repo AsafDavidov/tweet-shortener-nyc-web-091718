117fe68da47d2cdb193c1c889f95e6162bb6b984
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 18:00:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,16 @@
 # Write your code here.
 def dictionary
-  dictionary{
-    "hello"=>"hi"}
+  dictionary = {
+    "hello"=>"hi",
+    "to"=>"2",
+    "two"=>"2",
+    "too"=>"2",
+    "for"=>"4",
+    "four"=>"4",
+    "be"=>"b",
+    "you"=>"u",
+    "and"=>"&"
+  }
 end
 def word_substituter(string_tweets)
   substitions = dictionary
